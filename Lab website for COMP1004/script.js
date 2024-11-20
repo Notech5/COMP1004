9d617bd47a52ca5026b3b9fc6a7b82db49352a5f
Notech5 <noah.greenland@students.plymouth.ac.uk>
2024-11-20 10:52:16 +0000

    Lots of edits
    Edited the contents of the website, created a button which changes colour when the user hovers over it

---
+++ Lab website for COMP1004/script.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+
+document.getElementById('gotopage').addEventListener('click', function () {
+
+
+    window.location.href = "Class180.htm";
+
+});
+
+document.getElementById('gotopage').addEventListener('mouseover', function () {
+
+    this.style.backgroundColor = '#68A2F5';
+
+});
+
+document.getElementById('gotopage').addEventListener('mouseout', function () {
+
+    this.style.backgroundColor = '#007BFF';
+
+});
+
+elements = 
